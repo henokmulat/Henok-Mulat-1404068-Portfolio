6fc78ed8a9c244ab42d9f844537c205b6437b9b6
henok Mulat <murphysteven790@gmail.com>
2024-04-11 15:12:58 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const popupMenu = document.querySelector(".humburger-link");
+  const popupIcon = document.querySelector(".humburger-icon");
+  popupMenu.classList.toggle("open");
+  popupIcon.classList.toggle("open");
+}
